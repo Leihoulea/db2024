4d75dc163e2a2c43b76130315cd43c9c75df5d19
Leihoulea <107204131+Leihoulea@users.noreply.github.com>
2024-06-22 15:19:40 +0800

    Add files via upload

---
+++ majorcases.sql
@@ -0,0 +1,13 @@
+USE StudentManagement;
+
+INSERT INTO Majors (major_id, major_name) VALUES ('CS', 'Computer Science');
+INSERT INTO Majors (major_id, major_name) VALUES ('BIO', 'Biology');
+INSERT INTO Majors (major_id, major_name) VALUES ('CHEM', 'Chemistry');
+INSERT INTO Majors (major_id, major_name) VALUES ('MATH', 'Mathematics');
+INSERT INTO Majors (major_id, major_name) VALUES ('PSY', 'Psychology');
+INSERT INTO Majors (major_id, major_name) VALUES ('SOC', 'Sociology');
+INSERT INTO Majors (major_id, major_name) VALUES ('ANTH', 'Anthropology');
+INSERT INTO Majors (major_id, major_name) VALUES ('COMM', 'Communication Studies');
+INSERT INTO Majors (major_id, major_name) VALUES ('HIST', 'History');
+INSERT INTO Majors (major_id, major_name) VALUES ('PHIL', 'Philosophy');
+INSERT INTO Majors (major_id, major_name) VALUES ('ART', 'Art History');
